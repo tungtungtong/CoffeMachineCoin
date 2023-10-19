62074f9e40f0fd1487eff1102e12f795ae279ad0
RikaiPC <tungtungtong@gmail.com>
2023-10-19 14:48:23 +0900

    first commit

---
+++ data.py
@@ -0,0 +1,31 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 800,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 750,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 950,
+    }
+}
+
+resources = {
+    "water": 3000,
+    "milk": 2000,
+    "coffee": 1500,
+}
